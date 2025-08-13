adcca189e1002e9bfbc920a35d91b1c163eacab0
Wazed Ali <wazedulislam.567@gmail.com>
2025-08-14 02:28:39 +0600

    index.html

---
+++ scrept.js
@@ -0,0 +1,34 @@
+const result = document.getElementById("result");
+
+function display(number) {
+    result.value += number;
+}
+
+function calculate() {
+    try {
+        let final_number = result.value;
+   
+    final_number = final_number.replace(/(\d+)\s*\+\s*(\d+)%/g, (match,num, percent) => {
+        return Number(num) + (Number(num) * Number(percent) / 100);
+    });
+    final_number = final_number.replace(/(\d+)\s*\-\s*(\d+)%/g, (match,num, percnt) =>{
+        return Number(num) - (Number(num) * Number(percent)/ 100);
+    });
+    final_number = final_number.replace(/%/g, '/100');
+    const final_result = new Function(`return ${final_number}`)();
+    result.value = final_result;
+     // try , const nirapod from var
+        //const final_result = new Function(`return ${final_number}`)();
+        //result.value = final_result;
+    } catch (error) {
+        result.value = "Error";
+    }
+}
+
+function clrs() {
+    result.value = "";
+}
+
+function dle() {
+    result.value = result.value.slice(0, -1);
+}
